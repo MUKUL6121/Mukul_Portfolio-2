8ea5ebceb6265e2b0ef555bab156b1ef5a1a9929
Mukul Jangra <148455498+MUKUL6121@users.noreply.github.com>
2025-10-14 18:22:35 +0530

    Update header.jsx

--- src/component/Header/header.jsx
+++ src/component/Header/header.jsx
@@ -1,32 +1,44 @@
-import './header.css';
+import React from 'react';
 import { Link } from 'react-router-dom';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './header.css';
 
 function Header() {
-    return (
-        <div id="header-main">
-            <div id="Logo"><a href="/">Mukul</a></div>
+  return (
+    <div id="header-main">
+      <div id="Logo">
+        <Link to="/">Mukul</Link>
+      </div>
 
-            <ul id="ol">
-                <li><Link className="dropdown-item" to="/">Home</Link></li>
-                <li><Link className="dropdown-item" to="/Skills">Skills</Link></li>
-                <li><Link className="dropdown-item" to="/Projects">Projects</Link></li>
-                <li><Link className="dropdown-item" to="/Contact">Contact Me</Link></li>
-            </ul>
+      {/* Normal menu (desktop) */}
+      <ul id="ol">
+        <li><Link className="nav-link" to="/">Home</Link></li>
+        <li><Link className="nav-link" to="/Skills">Skills</Link></li>
+        <li><Link className="nav-link" to="/Projects">Projects</Link></li>
+        <li><Link className="nav-link" to="/Contact">Contact Me</Link></li>
+      </ul>
 
-            <div className="btn-group" id="header-dropdown">
-                <button type="button" className="btn btn-secondary dropdown-toggle"
-                    data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
-                    <i className="bi bi-list"></i>
-                </button>
-                <ul className="dropdown-menu dropdown-menu-end dropdown-menu-lg-start" id="toggle-item">
-                    <li><Link className="dropdown-item" to="/">Home</Link></li>
-                    <li><Link className="dropdown-item" to="/Skills">Skills</Link></li>
-                    <li><Link className="dropdown-item" to="/Projects">Projects</Link></li>
-                    <li><Link className="dropdown-item" to="/Contact">Contact Me</Link></li>
-                </ul>
-            </div>
-        </div>
-    )
+      {/* Dropdown menu (mobile) */}
+      <div className="btn-group" id="header-dropdown">
+        <button 
+          type="button" 
+          className="btn btn-secondary dropdown-toggle" 
+          data-bs-toggle="dropdown" 
+          aria-expanded="false"
+        >
+          <i className="bi bi-list"></i>
+        </button>
+        <ul className="dropdown-menu dropdown-menu-end">
+          <li><Link className="dropdown-item" to="/">Home</Link></li>
+          <li><Link className="dropdown-item" to="/Skills">Skills</Link></li>
+          <li><Link className="dropdown-item" to="/Projects">Projects</Link></li>
+          <li><Link className="dropdown-item" to="/Contact">Contact Me</Link></li>
+        </ul>
+      </div>
+    </div>
+  );
 }
+
 export default Header;
