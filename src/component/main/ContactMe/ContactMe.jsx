13d60ea5341a97dc9490f50005daf2025e721fa9
Mukul Jangra <148455498+MUKUL6121@users.noreply.github.com>
2025-10-16 05:43:31 +0530

    Update ContactMe.jsx

--- src/component/main/ContactMe/ContactMe.jsx
+++ src/component/main/ContactMe/ContactMe.jsx
@@ -2,8 +2,30 @@ import './ContactMe.css';
 import { useEffect, useState } from 'react';
 import { motion as MotionTag } from "framer-motion";
 import { useLocation } from 'react-router-dom';
+import axios from 'axios';
 
 function ContactMe() {
+    const [name_text, setname] = useState("");
+    const [mail, setmail] = useState("");
+    const [feedbacktext, setfeedback] = useState("");
+    const [confirm, setconfirm] = useState("");
+    async function submit_handle(e) {
+        e.preventDefault();
+        const url = "https://portfolio-backend-v7nt.onrender.com/post";
+        const name = name_text;
+        const email = mail;
+        const feedback = feedbacktext;
+        const data = { name, email, feedback };
+
+        await axios.post(url, data);
+        done();
+    };
+    function done() {
+        setmail("");
+        setfeedback("");
+        setconfirm("Feedback sent successfully!");
+    };
+
     const location = useLocation();
     const [Astyle, setStyle] = useState({});
 
@@ -21,20 +43,24 @@ function ContactMe() {
     }, [location.pathname]);
     return (
         <MotionTag.div id="contact-root" style={Astyle}>
+            {confirm}
             <div id="contact-container">
                 <h2>Contact Form</h2>
-                <form className='container' onSubmit={{}} style={{ width: "400px", border: "0px solid black" }}>
+                <form className='container' onSubmit={submit_handle} style={{ width: "400px", border: "0px solid black" }}>
                     <div>
                         <label htmlFor="" className='w-100'>Name :</label>
-                        <input type="text" className='w-100' />
+                        <input type="text" className='w-100' value={name_text}
+                            onChange={(e) => setname(e.target.value)} />
                     </div>
                     <div>
                         <label htmlFor="" className='w-50'>E-mail :</label>
-                        <input type="text" className='w-100' />
+                        <input type="mail" className='w-100' value={mail}
+                            onChange={(e) => setmail(e.target.value)} required/>
                     </div>
                     <div>
                         <label htmlFor="" className='w-50'>Comment :</label>
-                        <textarea type="text" className='w-100' />
+                        <textarea type="text" className='w-100' value={feedbacktext}
+                            onChange={(e) => setfeedback(e.target.value)} />
                     </div>
                     {/* btn */}
                     <div className='mt-2'><button>Send</button></div>
